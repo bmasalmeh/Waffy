2326cccc0cbe5cf7d07ae6e8cfdbdbbd61c3f68b
Batool AL-Masalmeh <batool.almasalmeh4@gmail.com>
2026-03-14 21:35:30 +0300

    first commit

---
+++ Waffy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Waffy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
